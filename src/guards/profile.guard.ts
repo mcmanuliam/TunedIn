004e946ce6de5d7d9523c7773ca2fd4ff359f47b
Liam McManus <liam@veryconnect.com>
2024-09-04 22:08:48 +0100

    refactor(Guards): refactor guards to include profile setup #8

---
+++ src/guards/profile.guard.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import type {UrlTree} from '@angular/router';
+import type {userProfile} from 'src/models/user';
+import {AbstractGuard} from './abstract.guard';
+
+// Continue to route if User hasn't setup-profile, else redirect to home
+@Injectable({providedIn: 'root'})
+export class AllowIfNotProfileSetup extends AbstractGuard {
+  protected redirectUrl(user: userProfile | null): UrlTree | true {
+    console.log(user?.profile_setup)
+    return user?.profile_setup ? this.router.createUrlTree(['/home']) : true;
+  }
+}
+
+// Continue to route if User has setup-profile, else redirect to sign-in.
+@Injectable({providedIn: 'root'})
+export class AllowIfProfileSetup extends AbstractGuard {
+  protected redirectUrl(user: userProfile | null): UrlTree | true {
+    return user?.profile_setup ? true : this.router.createUrlTree(['/profile-setup']);
+  }
+}
